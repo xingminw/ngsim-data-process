85f7bd3404d8180b9e725a249c2a21d90c742f22
Chenhao Zhang <cnhzhang@umich.edu>
2022-04-02 01:42:09 -0400

    Add application and interpolation methods

--- applications.py
+++ applications.py
@@ -1,14 +1,78 @@
+import os
+import mtldp.utils as mtlutils
+import mtltrajs as mtltrajs
+import mtlimg.img_classes as img
+
+timezone = "US/Eastern"
+output_folder = "output/traffic_images"
+
+
 # load data from either traffic matrices or trajectory points
+def generate_road_img_dict(network, overall_trajs_dict, start_tod, end_tod,
+                           temporal_interval, distance_interval, groupby_kwd="links"):
+    points_df = overall_trajs_dict.get_points_df(attributes="all")
+    points_df = mtlutils.df_add_date_time_and_tod(points_df, attr="timestamp", timezone_name=timezone)
+    date_trajs_df = dict(tuple(points_df.groupby(["date"])))
+    for date, daily_trajs_df in date_trajs_df.items():
+        print(f"Processing {date}...")
+        overall_points = mtltrajs.OverallPoints(df=daily_trajs_df)
+        daily_trajs_dict = overall_points.get_trajs_dict()
+        road_trajs_dict = daily_trajs_dict.groupby(groupby_kwd)
+        daily_road_img_dict = img.RoadImageDict(start_tod, end_tod, [date], temporal_interval, distance_interval,
+                                                groupby_kwd,
+                                                timezone_name=timezone, city_id="peachtree")
+        daily_road_img_dict.init_from_trajs_dict(road_trajs_dict, network)
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
 
-# todo: get full image
-density_matrix = img.get_path_density(road_list)
-speed_matrix = img.get_path_speed(road_list)
+        daily_road_img_dict.to_json(os.path.join(output_folder, f"output/{date}_traffic_image"))
 
-time_list = [0, 1, 2, 3, 4]
-# time_list = ['00:00', '00:02']
-for i_time in time_list:
-    link_id = "XXX"             # segment_id,...
-    # todo: some functions to extract data from traffic images
-    density_vector = img.get_density_vec(link_id, i_time)
-    speed_vector = img.get_density_vec(link_id, i_time)
-    cell_length_list = img.get_cell_length_list(link_id)
+
+def load_road_img_dict():
+    """
+
+    :return: `dict`, {date: RoadImageDict}
+    """
+    date_road_dict = {}
+    if not os.path.exists(output_folder):
+        print(f"No data found in {output_folder}")
+        return None
+
+    file_name_list = os.listdir(output_folder)
+    for file_name in file_name_list:
+        date = file_name.split("_")[0]
+        date_road_dict[date] = img.read_road_image_dict_from_json(file_name)
+
+    return date_road_dict
+
+
+def aggregate_road_image_dicts(road_dict_list):
+    """
+
+    :param road_dict_list: list of RoadImageDict
+    :return: RoadImageDict
+    """
+    aggregated_road_image_dict = img.assign_weights_to_rimg_dicts(road_dict_list, weights=1.0 / len(road_dict_list))
+    return aggregated_road_image_dict
+
+
+def application(rdimg_dict, road_id_list, time_list):
+    """
+
+    :param rdimg_dict: list of RoadImageDict
+    :param road_id_list: list of road_id
+    :param time_list: list of date time string
+    :return:
+    """
+    density_matrix = rdimg_dict.get_path_density(road_id_list, remove_incomplete_cell=True)
+    speed_matrix = rdimg_dict.get_path_speed(road_id_list, remove_incomplete_cell=True)
+    flow_matrix = rdimg_dict.get_path_flow(road_id_list, remove_incomplete_cell=True)
+
+    for road_id, road_image in rdimg_dict.dict.items():
+        for time_point in time_list:
+            density_vector = road_image.get_density_vector(road_id, time_point, remove_incomplete_cell=True)
+            speed_vector = road_image.get_speed_vector(road_id, time_point, remove_incomplete_cell=True)
+            flow_vector = road_image.get_flow_vector(road_id, time_point, remove_incomplete_cell=True)
+
+if __name__ == "__main__":
+    pass
--- interpolation.py
+++ interpolation.py
@@ -13,18 +13,21 @@ corridor = mtlmap.Arterial(network, 'peachtree',
                            input_dict=arterial_info_dict, putin_network=True,
                            ref_node='69421277')
 
-
 points_df = pd.read_csv('peachtree/matched_trajs.csv')
 points_table = mtltrajs.OverallPoints()
 points_table.load_data(points_df)
 trajs_dict = points_table.get_trajs_dict(groupby='traj_id',
                                          traj_attributes=['link_id', 'movement_id', 'junction_id'])
 
+# todo: call function get the traffic matrices, in application
+# todo: interpolation: for every 1 second (only keep new points)
+numeric_column = ["distance", "speed", "longitude", "latitude"]
+for trip_id, traj in trajs_dict.dict.items():
+    new_traj = mtltrajs.interpolation(traj, numeric_column, resolution=1, degree=1)
+    new_traj.df = new_traj.df.loc[new_traj.df["inserted"] == 1]
+    new_traj.df = new_traj.df.drop(columns="inserted")
+    trajs_dict.dict[trip_id] = new_traj
+
 print()
 
-# todo: call function get the traffic matrices
-# todo: interpolation: for every 1 second (only keep new points)
-
-points_df = trajs_dict.get_points_df(attributes='all')
-
-
+# points_df = trajs_dict.get_points_df(attributes='all')
